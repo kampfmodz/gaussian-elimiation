a1093155af447b0879fc4f9df0f1a0dc777e51d5
kampfmodz <contact@kampfmodz.de>
2025-07-07 23:04:34 +0200

    feat(examples): added example4, cleanup example3

--- examples.c
+++ examples.c
@@ -46,12 +46,13 @@ double *example2(double **matrix, int n)
     return solve(matrix, n);
 }
 
-double *example3(double **matrix_, int n)
+double *example3(double **matrix, int n)
 {
-    double **matrix = (double **)malloc(2 * sizeof(double *));
-    for (int i = 0; i < 2; i++)
+    n = 2;
+    matrix = (double **)malloc(n * sizeof(double *));
+    for (int i = 0; i < n; i++)
     {
-        matrix[i] = (double *)malloc((2 + 1) * sizeof(double));
+        matrix[i] = (double *)malloc((n + 1) * sizeof(double));
     }
 
     // matrix daten
@@ -65,5 +66,28 @@ double *example3(double **matrix_, int n)
     matrix[0][2] = -3;
     matrix[1][2] = 1;
 
-    return solve(matrix, 2);
+    return solve(matrix, n);
+}
+
+double *example4(double **matrix, int n)
+{
+    // matrix daten
+    // erst Zeile, dann Spalte
+    matrix[0][0] = -1;
+    matrix[1][0] = 1;
+    matrix[2][0] = 5;
+
+    matrix[0][1] = 1;
+    matrix[1][1] = -3;
+    matrix[2][1] = 1;
+
+    matrix[0][2] = 1;
+    matrix[1][2] = -2;
+    matrix[2][2] = 4;
+
+    matrix[0][3] = 0;
+    matrix[1][3] = 5;
+    matrix[2][3] = 3;
+
+    return solve(matrix, n);
 }
